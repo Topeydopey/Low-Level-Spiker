5ba2e82804ddfb4cb20d4c0e0100c838c0cb9264
Topey <topeydopey@gmail.com>
2024-12-12 05:28:38 +0700

    Step 4 Pathmaker Attributes
    -Added an array and a PlaceTile
    -Added a function that spawns objects in the array
    -Imported 2 extra test squares
    -Added lifetime for pathmakers, now will live for their random range of tile spawned
    -Changed max tile to 1500

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Pathmaker : MonoBehaviour
 {
     private int counter = 0;
-    public Transform floorPrefab;
-    public Transform pathmakerSquarePrefab;
+    private int maxLifetime; // The lifetime for pathmaker
+    public Transform[] floorPrefabs; //Array for types of floor
+    //public Transform floorPrefab;
+    public Transform pathmakerPrefab;
     public LayerMask tileLayerMask; // Filter to check objects only on specific layers
 
     // "static" means a variable is shared across all instances
     public static int globalTileCount = 0; // Tracks global tile count
-    public static int maxTiles = 500; // Maximum global tile count
+    public static int maxTiles = 1500; // Maximum global tile count
+
+    void PlaceTile()
+    {
+        int randomIndex = Random.Range(0, floorPrefabs.Length); // Random.Range will generates a valid index from 0 to the (array - 1)
+        Instantiate(floorPrefabs[randomIndex], transform.position, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        maxLifetime = Random.Range(1000, 2000); // path maker spawns min / max amount of tile before destroying itself (maximum tile limit still applies)
+    }
     void Update()
     {
-        if (counter < 50 && globalTileCount < maxTiles)  // Counter that tracks the pathmaker tile spawn count
+        if (counter < maxLifetime && globalTileCount < maxTiles)  // Counter that tracks the pathmaker tile spawn count
         // Explaination: if counter is less than 50 AND globaltile is less than maxtile then execute if command
         {
             float randomNumber = Random.Range(0f, 1f);
@@ -28,11 +41,12 @@ public class Pathmaker : MonoBehaviour
             }
             else if (randomNumber < 0.99f)
             {
-                Instantiate(pathmakerSquarePrefab, transform.position, Quaternion.identity); // Spawn pathmaker at current location
+                Instantiate(pathmakerPrefab, transform.position, Quaternion.identity); // Spawn pathmaker at current location
             }
             if (!Physics2D.OverlapCircle(transform.position, 0.1f, tileLayerMask))
             {
-                Instantiate(floorPrefab, transform.position, Quaternion.identity); // Spawn a floor tile}
+                //Instantiate(floorPrefab, transform.position, Quaternion.identity); // Spawn a floor tile}
+                PlaceTile();
                 globalTileCount++;
             }
             transform.Translate(Vector2.up); // Move up X units (in 2D)
