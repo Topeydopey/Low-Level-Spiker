14fe66134474ff6510147180d334445e185806b2
Topey <topeydopey@gmail.com>
2024-12-12 03:33:34 +0700

    Step 3 Implementation
    -Successfully implemented code and tested it
    # Tiles overlaps, will be fixed in later steps

--- Assets/Scripts/Pathmaker.cs
+++ Assets/Scripts/Pathmaker.cs
@@ -8,9 +8,13 @@ public class Pathmaker : MonoBehaviour
     public Transform floorPrefab;
     public Transform pathmakerSquarePrefab;
 
+    // "static" means a variable is shared across all instances
+    public static int globalTileCount = 0; // Tracks global tile count
+    public static int maxTiles = 500; // Maximum global tile count
     void Update()
     {
-        if (counter < 50)
+        if (counter < 50 && globalTileCount < maxTiles)  // Counter that tracks the pathmaker tile spawn count
+        // Explaination: if counter is less than 50 AND globaltile is less than maxtile then execute if command
         {
             float randomNumber = Random.Range(0f, 1f);
             if (randomNumber < 0.25f)
@@ -26,8 +30,9 @@ public class Pathmaker : MonoBehaviour
                 Instantiate(pathmakerSquarePrefab, transform.position, Quaternion.identity); // Spawn pathmaker at current location
             }
             Instantiate(floorPrefab, transform.position, Quaternion.identity); // Spawn a floor tile
-            transform.Translate(Vector2.up * 5f); // Move up 5 units (in 2D)
+            transform.Translate(Vector2.up); // Move up X units (in 2D)
             counter++; // add to the counter
+            globalTileCount++;
         }
         else
         {
